4868876df89ee37c4de957fd9027d4b33a6eef5c
jon brookes <marshyon@gmail.com>
2017-06-01 21:53:42 +0100

    added mongo db to store data for graph

--- imports/ui/DoughnutWorld.js
+++ imports/ui/DoughnutWorld.js
@@ -2,34 +2,60 @@ import React from 'react';
 
 
 import {Doughnut} from 'react-chartjs-2';
-
-let dougnuts = {
-	labels: [
-		'Red',
-		'Green',
-		'Yellow'
-	],
-	datasets: [{
-		data: [300, 50, 100],
-		backgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		],
-		hoverBackgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		]
-	}]
-};
+import {DoughnutData} from '../api/DoughnutData';
 
 export default class DoughnutWorld extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numData: []
+    };
+  }
+  componentDidMount() {
+    this.numsTracker = Tracker.autorun(() => {
+      const nums  = DoughnutData.find().fetch();
+      const numData = [];
+      nums.forEach( function (arrayItem, i) {
+          numData[i] = arrayItem.data;
+      });
+      this.setState({ numData });
+    });
+  }
+  componentWillUnmount() {
+    this.numsTracker.stop();
+  }
+
+  getDoughnutData() {
+    const ddata = this.state.numData;
+    const dougnuts = {
+      labels: [
+        'Red',
+        'Green',
+        'Yellow'
+      ],
+      datasets: [{
+        data: ddata,
+        backgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
+        ]
+      }]
+    };
+    return dougnuts;
+  }
+
   render() {
     return (
       <div>
         <h2>Doughnut Example</h2>
-        <Doughnut data={dougnuts} />
+        <Doughnut data={this.getDoughnutData()} />
       </div>
     );
   }
