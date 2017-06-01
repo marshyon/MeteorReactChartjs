bb4ecb61791bc5f7f00361ffd1a81457ce6974e5
jon brookes <marshyon@gmail.com>
2017-06-01 21:53:55 +0100

    added mongo db to store data for graph

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+// import DoughnutData from '../imports/api/DoughnutData.js';
+import DoughnutData from '../imports/api/DoughnutData';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
